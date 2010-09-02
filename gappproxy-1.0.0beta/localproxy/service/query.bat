cdb6f0c48a2edd4ccea98f820cdff3dbb2e409c3
zy.zorro@gmail.com <zy.zorro@gmail.com@0da7ae3a-8bc9-11de-b841-6521bc8c149b>
2010-09-02 08:42:59 +0000

    create a branch for cygwin

--- gappproxy-1.0.0beta/localproxy/service/query.bat
+++
@@ -1,4 +0,0 @@
-:: In Windows xp, 'sc query' will always exit with status code '0',
-:: no matter the query faild or not.
-
-sc query GAppProxy | find "SERVICE_NAME: GAppProxy"
--- rdp/X11R6.9.0/doc/specs/X11/indexmacros.t
+++
@@ -1,3 +0,0 @@
-.eh '\fBXlib \- C Library\fP''\fBX11, Release 6.9/7.0\fP'
-.oh '\fBXlib \- C Library\fP''\fBX11, Release 6.9/7.0\fP'
-.so index.pageno
